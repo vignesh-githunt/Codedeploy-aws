f4df724b84dbb175a111c5a95ec3df9e682efc49
vignesh-hu <64769421+vignesh-hu@users.noreply.github.com>
2020-10-07 00:54:28 +0530

    Delete prebuild.sh

--- prebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Giving permission for Building_Patch_Files.sh"
-chmod 777 Building_Patch_Files.sh
-echo "Executiing Building_Patch_Files.sh"
-./Building_Patch_Files.sh
-echo "checking is the patches are builded"
-find opt/ -type f -mmin -10  | awk '{print $1}'
-tar -cvf opt.tar.gz opt
